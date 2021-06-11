b3138d9005e40e8ba0ded1d43aae8c5cd8c2dad0
Obi <mr.obiume@gmail.com>
2021-06-11 14:36:53 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+const greeting = [];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    greeting.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+return greeting;
+}
+
+function countDown() {
+    let counter = 10;
+    while (counter >= 0) {
+        console.log(counter--);
+    }
+}
